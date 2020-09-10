8d683e554bbfc4acd252cf32909f36261aea170b
justinonstot <justinmonstot@gmail.com>
2020-09-09 22:17:20 -0700

    create scratchpad for testing

--- find_in_file.py
+++ find_in_file.py
@@ -7,17 +7,23 @@ myfile = file_path + file_name
 searchWord = 'louis'
 successCounter = 0
 
+# We will accumulate the lines of interest in a list for later usage
+lineBuilder = []
+
 if path.exists(myfile):
-    # print("File exists: " + myfile)
     fh = open(file_path + file_name)
     for line in fh:
         if line.startswith('From ') and searchWord not in line:
              pass
         elif line.startswith('From ') and searchWord in line:
-             print(line)
-             successCounter +=1
+            line = line.rstrip()
+            print(line)
+            lineBuilder.append(line) # var is not used currently but may need in future
+            successCounter +=1
     if successCounter == 0:
         print('\n Search Term Was NOT found in file!\n')
-
+    
+    # print("\nResults of query: \n", lineBuilder)
+    
 else:
     print("\nERROR ENCOUNTERED: File not found!\n")
---
+++ scratchpad.py
@@ -0,0 +1,8 @@
+a = 'banana'
+b = 'split'
+
+c = a
+
+print(id(b))
+print(id(c))
+print(id(a))
